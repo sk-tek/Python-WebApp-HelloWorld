cf56c0b8517cdb21485226c988cbe51cc6dbfdd2
sk-tek <skloud.tech@gmail.com>
2026-06-07 14:01:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return {
+        "status": "success",
+        "message": "Hello World"
+    }
+
+@app.route("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=8080
+    )
